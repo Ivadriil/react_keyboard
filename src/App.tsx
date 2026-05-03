e9301a43b9d8a9d8eed6e4ffbc2427dfb3c1eb07
Ivadriil <petrivi51@gmail.com>
2026-05-03 12:43:00 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export const App: React.FC = () => {
-  const [key, setkey] = useState('');
-  const [keytru, setKeytru] = useState(false);
-
-  document.addEventListener('keyup', (event: KeyboardEvent) => {
-    setkey(event.key);
-    setKeytru(true);
-  });
-
-  return (
-    <div className="App">
-      {keytru && (
-        <p className="App__message">The last pressed key is [{key}]</p>
-      )}
-      {!keytru && <p className="App__message">Nothing was pressed yet</p>}
-    </div>
-  );
+type State = {
+  key: string;
 };
+
+export class App extends React.Component {
+  state: State = {
+    key: '',
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ key: event.key });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ key: event.key });
+    });
+  }
+
+  render() {
+    const { key } = this.state;
+
+    return (
+      <div className="App">
+        {key && <p className="App__message">The last pressed key is [{key}]</p>}
+        {!key && <p className="App__message">Nothing was pressed yet</p>}
+      </div>
+    );
+  }
+}
